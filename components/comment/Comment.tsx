5d87df6817bdb1b9f0798176aa88a78fb0bd3eb0
Carlos Jarero <lcjm@thinkcares.com>
2022-08-23 22:01:48 -0500

    reverse mode filter

--- components/comment/Comment.tsx
+++ components/comment/Comment.tsx
@@ -7,11 +7,11 @@ interface Icomment {
 }
 const Comment: FunctionComponent<Icomment> = ({ author, message, date }) => {
   return <div className="w-full px-[20px] py-[20px] rounded-2xl bg-[#0FC2C0] my-[10px]" >
-    <div className="flex w-full justify-between items-center" >
+    <div className="flex w-full justify-between items-center m-0 " >
     <p className="text-white text-[16px] pr-[12px] lg:pr-0  lg:text-xl font-bold" > { author } </p>
     <p className="text-white text-[10px] italic lg:text-sm " > { date.toLocaleString() } </p>
     </div>
-    <p className="text-white pt-[20px] text-[16px] text-xl " > { message } </p>
+    <p className="text-white pt-[20px] m-0 text-[16px] text-xl " > { message } </p>
   </div>;
 };
 
